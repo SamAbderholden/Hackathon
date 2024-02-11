7d570d2cbc3ce1aa628927775f73eb92d92b108e
Brendan <bmoroney@mines.edu>
2024-02-10 19:01:36 -0700

    changed medicare

--- medicaid.py
+++ medicaid.py
@@ -25,13 +25,16 @@ def get_data(location):
         'Sec-Fetch-Mode': 'cors',
         'Sec-Fetch-Site': 'same-origin',
         'Set-Cookie':"ak_bmsc=127B035B1DA1A234EF1C1D48DEC3AFA1~000000000000000000000000000000~YAAQR40hF52gOoWNAQAA+dZ+lRa4TqDYB/G8dQhfDfpwZ5rRBGrbQuYuhXXM9JAMsnhjkWEacQFi9/f2Gs0MuD0cVhuK1+so0pmKjCItk/18loL5IWtb5eWxBgzFs23+YQpfzAPwvf6NhvRS2v3QceVve/MNWF/jU5LfcEk0I3/yb6Oly5F8p0oTFrkBcFV/YUq9iUILy3a6pfrjR9XozA6musP6zkJ1avPoMwoCjj5cZGW01I+td5h8ax1JOwAIMW22U9mRIwCrVD8SxBWD/66EZtmIpOyfhPnh6khNyQaPRZkMclkfwjbKmINZSSlCSkw4Y7NJ3XvsL8koEQlvJaHAwdtG6OXUyP8+QCFlZBOoJcQTB/B2De1NBzeyLnffTVhUq9HP8DRr; Domain=.my.site.com; Path=/; Expires=Sun, 11 Feb 2024 02:08:52 GMT; Max-Age=7198; HttpOnly",
-        'Referrer-Policy':'origin-when-cross-origin'
+        'Referrer-Policy':'origin-when-cross-origin',
+         "User-Agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:109.0) Gecko/20100101 Firefox/115.0",
+         "X-Requested-With":"XMLHttpRequest",
+        "X-User-Agent":"Visualforce-Remoting"
+
         }
     payload = {
         "action":"ProviderSearchController",
         "method":"getProviders",
-        "ver":"45",
-        "vid" : "0660H000005RR6P",
+        
         "data":[{"apexType":"c.ProviderSearchController.ProviderSearchFilters",
                  "selectedLatitude":lat,
                  "selectedLongitude":long,
@@ -44,11 +47,14 @@ def get_data(location):
                  "waiverType":"",
                  "waiverServices":""},0],
         "type":"rpc",
-        "ns":"",
-        "tid":5,
-        "ctx":{"csrf":"VmpFPSxNakF5TkMwd01pMHhNMVF5TXpvME5UbzFNeTQzTnpWYSxMa2FFRERUc2JiRFJzelhkWHQ2VHZxM296bHZkVUtrSTRPUDdpUlAxQTVJPSxaR1V6TlRCaQ=="},  
-        "authorization":"eyJub25jZSI6ImxRZndPSUJGVEZOUE82OFExUVB4UkxFcmRWUVlVc1o5TGR4UXZBUEFfWFlcdTAwM2QiLCJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiIsImtpZCI6IntcInRcIjpcIjAwRGkwMDAwMDAwSVB5UFwiLFwidlwiOlwiMDJHMEgwMDAwMDA0cWpjXCIsXCJhXCI6XCJ2ZnJlbW90aW5nc2lnbmluZ2tleVwiLFwidVwiOlwiMDA1MEgwMDAwMEQ0djd6XCJ9IiwiY3JpdCI6WyJpYXQiXSwiaWF0IjoxNzA3NjA4NzUzNzc3LCJleHAiOjB9.Q2lWUWNtOTJhV1JsY2xObFlYSmphRU52Ym5SeWIyeHNaWEl1WjJWMFVISnZkbWxrWlhKeg==.pOJtdjBY5K1UfYR_V76oZ10iQRQpBgtjjU1",
-
+        "tid":2,
+        "ctx":{
+            "csrf":"VmpFPSxNakF5TkMwd01pMHhNMVF5TXpvME5UbzFNeTQzTnpWYSxMa2FFRERUc2JiRFJzelhkWHQ2VHZxM296bHZkVUtrSTRPUDdpUlAxQTVJPSxaR1V6TlRCaQ==",  
+            "vid" : "0660H000005RR6P",
+            "ns" : "",
+            "ver":45,
+            "authorization":"eyJub25jZSI6ImxRZndPSUJGVEZOUE82OFExUVB4UkxFcmRWUVlVc1o5TGR4UXZBUEFfWFlcdTAwM2QiLCJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiIsImtpZCI6IntcInRcIjpcIjAwRGkwMDAwMDAwSVB5UFwiLFwidlwiOlwiMDJHMEgwMDAwMDA0cWpjXCIsXCJhXCI6XCJ2ZnJlbW90aW5nc2lnbmluZ2tleVwiLFwidVwiOlwiMDA1MEgwMDAwMEQ0djd6XCJ9IiwiY3JpdCI6WyJpYXQiXSwiaWF0IjoxNzA3NjA4NzUzNzc3LCJleHAiOjB9.Q2lWUWNtOTJhV1JsY2xObFlYSmphRU52Ym5SeWIyeHNaWEl1WjJWMFVISnZkbWxrWlhKeg==.pOJtdjBY5K1UfYR_V76oZ10iQRQpBgtjjU1",
+            }
         }
     response = requests.post(url, json=payload, headers=headers)
     data = response.json()
